322a96ec1c0c3da4efce178d9325a788914996dd
Mouhsine Rouaki <houtm27@gmail.com>
2025-10-29 12:23:59 +0100

    ajout de function crud en js

--- js/script.js
+++ js/script.js
@@ -7,25 +7,69 @@ const cardAjout = document.getElementById("formAjout");
 const months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
 const date = new Date();
 
+class Reservation{
+    constructor({id = `${date.getFullYear()}-${date.getMonth()}-${date.getDate()}` , titre, description ,heureBedut,heureFin,nbPersone,type}){
+        this.id = id;
+        this.titre = titre;
+        this.description = description;
+        this.heureBedut = heureBedut;
+        this.heureFin = heureFin;
+        this.nbPersone = nbPersone;
+        this.type = type;
+    };
+};
+
+let restauration = [];
+
+localStorage.setItem("restauration" ,JSON.stringify(restauration));
+
+function refreshRestauration(restauration){
+    restauration = JSON.parse(localStorage.getItem("reservations"));
+}
+refreshRestauration(restauration);
+
+function addReservation(reservation) {
+    restauration.push(reservation);
+    localStorage.setItem("restauration" ,JSON.stringify(restauration));
+};
+function getReservations(idReservation){
+    let listReservation  = [];
+    for(let i = 0 ; i < restauration.length; i++){
+        if(restauration[i].id == idReservation){
+            listReservation.push(restauration[i])
+        }
+    }
+    return listReservation;
+}
+
+function modiffierReservation(idReservation, titre, description, heureBedut, heureFin, nbPersone, type) {
+    for(let i = 0 ; i < restauration.length; i++){
+        if(restauration[i].id == idReservation){
+            restauration.titre = titre;
+            restauration.description = description;
+            restauration.heureBedut = heureBedut;
+            restauration.heureFin = heureFin;
+            restauration.nbPersone = nbPersone;
+            restauration.type = type;
+        }
+    }
+    refreshRestauration(restauration);
+}
+function supprimerReservation(idReservation){
+    for(let i = 0 ; i < restauration.length; i++){
+        if(restauration[i].id == idReservation){
+            delete restauration[i]
+        }
+    }
+    refreshRestauration(restauration);
+}
+
 function updateMonthYear(){
     month.innerHTML = `${months[date.getMonth()]} ${date.getFullYear()}`;
 }
 updateMonthYear();
 
-function drag(ev) {
-  ev.dataTransfer.setData("text", ev.target.id);
-}
 
-function allowDrop(ev) {
-  ev.preventDefault();
-}
-
-function drop(ev) {
-  ev.preventDefault();
-  const id = ev.dataTransfer.getData("text");
-  const element = document.getElementById(id);
-  ev.target.appendChild(element);
-}
 
 function aficherCard(){
     const divCalendrier = document.querySelectorAll(".days div");
@@ -51,11 +95,10 @@ function aficherCard(){
                 const type = document.getElementById("type").value;
 
                 const nouvelleReservation = new Reservation({titre,description,heureBedut,heureFin,nbPersone,type});
-                console.log(nouvelleReservation.titre)
                 addReservation(nouvelleReservation);
+                let reseravtions = getReservations(nouvelleReservation.id);
                 const p = document.createElement("p");
                 p.setAttribute('draggable' , 'true');
-                p.setAttribute('ondragstart' , 'dragstartHandler(event)');
                 p.textContent = titre;
                 p.style.backgroundColor = "red" ;
                 divCalendrier[i].append(p);
@@ -65,30 +108,41 @@ function aficherCard(){
     }
 }
 
-
-
-
-
-
 function Gourmet(){
     date.setDate(1);
     const firstDay = new Date(date.getFullYear(), date.getMonth(), 1).getDay();
     const lastDay = new Date(date.getFullYear(), date.getMonth()+1, 0).getDate();
     const derniereMonthDay = new Date(date.getFullYear(), date.getMonth(),0).getDate();
+
     for(let i = firstDay; i > 0;i--){
         const dayDiv = document.createElement("div");
         dayDiv.id = `${date.getFullYear()}-${date.getMonth()-1}-${derniereMonthDay -i +1}`;
-        dayDiv.setAttribute('ondrop' , 'drop(event)');
-        dayDiv.setAttribute('ondragover' , 'allowDrop(event)');
         dayDiv.textContent = derniereMonthDay -i +1;
+        let resrvationDay=getReservations(dayDiv.id);
+        if(resrvationDay.length !=  0){
+            for(let j = 0 ; j < resrvationDay.length ;j++){
+                let p = document.createElement("p");
+                p.textContent = resrvationDay[j].titre;
+                p.style.backgroundColor = "red"
+                dayDiv.append(p);
+            }
+        }
         Containerdays.append(dayDiv);
     }
     for(let i = 1; i<= lastDay;i++){
         const dayDiv = document.createElement("div");
         dayDiv.textContent =  i ;
         dayDiv.id = `${date.getFullYear()}-${date.getMonth()}-${i}`;
-        dayDiv.setAttribute('ondrop' , 'drop(event)');
-        dayDiv.setAttribute('ondragover' , 'allowDrop(event)');
+        let resrvationDay=getReservations(dayDiv.id);
+        if(resrvationDay.length !=  0){
+            for(let j = 0 ; j < resrvationDay.length ;j++){
+                let p = document.createElement("p");
+                p.textContent = resrvationDay[j].titre;
+                p.style.backgroundColor = "red"
+                dayDiv.append(p);
+                
+            }
+        }
         Containerdays.append(dayDiv);
     } 
 }
@@ -118,30 +172,9 @@ prevbtn.addEventListener('click' ,function(){
 
 aficherCard();
 
-class Reservation{
-    constructor({id = `${date.getFullYear()}-${date.getMonth()}-${date.getDate()}` , titre, description ,heureBedut,heureFin,nbPersone,type}){
-        this.id = id;
-        this.titre = titre;
-        this.description = description;
-        this.heureBedut = heureBedut;
-        this.heureFin = heureFin;
-        this.nbPersone = nbPersone;
-        this.type = type;
-    };
-};
 
-let restauration = [];
-function addReservation(reservation) {
-    restauration.push(reservation)    
-}
-function ModiffierReservation(reservation, titre, description, heureBedut, heureFin, nbPersone, type) {
-  reservation.titre = titre;
-  reservation.description = description;
-  reservation.heureBedut = heureBedut;
-  reservation.heureFin = heureFin;
-  reservation.nbPersone = nbPersone;
-  reservation.type = type;
-}
+
+
 
   
 
